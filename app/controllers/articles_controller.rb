24f14d9936a1d7299c791ad9be541c1c37ed8978
rwparrish <rino3717@gmail.com>
2020-12-16 13:17:05 -0700

    practice byebug

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,7 @@
 class ArticlesController < ApplicationController
 
   def show
+    # byebug - built in Ruby debugger used to freeze the program so data can be expossed where it is placed
     @article = Article.find(params[:id])
   end
 
