e9ffaa29c3e3f373b2de65bef796410c37192e3e
rwparrish <rino3717@gmail.com>
2020-12-16 13:29:07 -0700

    working index

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,9 @@
 class ArticlesController < ApplicationController
 
+  def index
+    @articles = Article.all
+  end
+
   def show
     # byebug - built in Ruby debugger used to freeze the program so data can be expossed where it is placed
     @article = Article.find(params[:id])
