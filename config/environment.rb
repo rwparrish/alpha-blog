c31086bbe9d1650c25048b43e840e38cae2e64a5
rwparrish <rino3717@gmail.com>
2020-12-28 11:28:36 -0700

    Update environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,8 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+# code below will remove the field_with_errors class provided by rails since I am using bootstrap 
+ActionView::Base.field_error_proc = Proc.new do |html_tag, instance|
+  html_tag.html_safe
+end
