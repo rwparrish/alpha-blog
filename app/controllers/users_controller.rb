d79bd085b417659ae01a96a2a6560f8def20bf4b
rwparrish <rino3717@gmail.com>
2021-01-15 08:36:33 -0700

    add users index page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,10 @@ class UsersController < ApplicationController
 
   before_action :set_user, only: [:show, :edit, :update, :destroy]
 
+  def index
+    @users = User.all
+  end
+
   def show
     @articles = @user.articles
   end
