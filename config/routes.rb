18426790d050a6ee6ae3d67c1fc6962673aa1f96
rwparrish <rino3717@gmail.com>
2021-01-27 10:36:29 -0700

    add sessions routes and controller

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   resources :articles
   get 'signup', to: 'users#new'
   resources :users, except: [:new]
-  get 'login', to: 'sessiosn#new'
+  get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
-  delete 'logout', to: 'sessiosn#destroy'
+  delete 'logout', to: 'sessions#destroy'
 end
