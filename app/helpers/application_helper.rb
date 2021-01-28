f0e92ac761a801661baa2d6b46cd2ca4f18dbc0a
rwparrish <rino3717@gmail.com>
2021-01-28 10:06:14 -0700

    add helper methods to application controller

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,15 +8,4 @@ module ApplicationHelper
     image_tag(gravatar_url, alt: user.username, class: "rounded shadow mx-auto d-block")
   end
 
-  def current_user
-    # if a @current_user already exists then simply return it,
-    # if not, then find the @current_user if there is a session @obj containing a user_id
-    @current_user ||= User.find(session[:user_id]) if session[:user_id]
-  end
-
-  def logged_in?
-    # turn the return of the current_user method into a boolean using the !!
-    !!current_user
-  end
-
 end
